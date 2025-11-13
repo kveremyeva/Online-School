9f25ce287b6e3e28275b733efbb64f5e960eb395
Ksenia Veremyeva < >
2025-11-13 23:08:47 +0300

    Домашная работа 31

--- users/serializers.py
+++ users/serializers.py
@@ -11,14 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'password2', 'phone', 'city')
+        fields = ('email', 'password', 'phone', 'city')
 
     def create(self, validated_data):
-        validated_data.pop('password2')
         password = validated_data.pop('password')
         user = User(**validated_data)
         user.set_password(password)
