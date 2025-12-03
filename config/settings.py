dca15af34d4c8ae1b31f00160994204e6c6b83df
Ksenia Veremyeva < >
2025-12-03 03:08:06 +0300

    Домашная работа 34.2

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
         'PORT': os.getenv('PORT'),
     }
 }
